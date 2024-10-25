8c5572960fa29f0ae1db9f0ec7e7412887dcc3bd
Erlangga4610 <gilangerlangga1306@gmail.com>
2024-10-25 14:46:49 +0700

    commit baru

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Component;
+use App\Livewire\Dashboard;
 
+// Route untuk halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', Dashboard::class);
